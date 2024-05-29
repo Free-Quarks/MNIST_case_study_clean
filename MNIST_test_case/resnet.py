a7d0b8b38e7b3f541455d3969364eb868586bdf1
Free-Quarks <lieffers@arizona.edu>
2024-05-29 12:28:08 -0700

    new training loop and tensorboard writer

--- MNIST_test_case/resnet.py
+++ MNIST_test_case/resnet.py
@@ -3,15 +3,11 @@ from torch import nn
 from torchvision.models import resnet18
 from train import DatasetWrapper
 from torch.utils.data import DataLoader
-# use ignite for training this model
-from ignite.engine import Events, create_supervised_trainer, create_supervised_evaluator
-from ignite.metrics import Accuracy, Loss
-from ignite.contrib.handlers import TensorboardLogger, global_step_from_engine
-from ignite.handlers.tensorboard_logger import OutputHandler
+from torch.utils.tensorboard import SummaryWriter
 
 # config
-DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-MAX_EPOCHS = 5
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu') # 'cuda:0' if only one gpu
+MAX_EPOCHS = 7
 BATCH_SIZE = 128
 LOG_ITER = 75
 
@@ -37,61 +33,37 @@ train_dataset_9 = torch.load('./dataset/MNIST/modified/train_dataset_9.pt')
 train_loader_9less = DataLoader(dataset=train_dataset_9less, batch_size=BATCH_SIZE, shuffle=True)
 train_loader_9 = DataLoader(dataset=train_dataset_9, batch_size=BATCH_SIZE, shuffle=True)
 
-# initialize model
-model = R18().to(DEVICE)
+# initialize model and parallelization across GPU's
+model = R18()
+model = torch.nn.DataParallel(model)
+model.to(DEVICE)
 
 # set up optmiizer and loss function
-optimizer = torch.optim.RMSprop(model.parameters(), lr=0.005)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.005)
 criterion = nn.CrossEntropyLoss()
 
-# now to create trainers and evaluators 
-trainer = create_supervised_trainer(model, optimizer, criterion, DEVICE)
+# set up tensorboard writer
+writer = SummaryWriter()
 
-eval_metrics = {
-    "accuracy": Accuracy(),
-    "loss": Loss(criterion)
-}
+for epoch in range(MAX_EPOCHS):
+    model.train()
+    running_loss = 0.0
 
-train_evaluator = create_supervised_evaluator(model, metrics=eval_metrics, device=DEVICE)
-#val_evaluator = create_supervised_evaluator(model, metrics=val_metrics, device=DEVICE)
+    for i, (inputs, labels) in enumerate(train_loader_9):
+        inputs, labels = inputs.to(DEVICE), labels.to(DEVICE)
 
-######## LOGGING ########
-# intermediate results every 75 iterations
-@trainer.on(Events.ITERATION_COMPLETED(every=LOG_ITER))
-def log_training_loss(engine):
-    print(f"Epoch[{engine.state.epoch}], Iter[{engine.state.iteration}] Loss: {engine.state.output:.2f}")
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        writer.add_scalar("Loss/train", loss, epoch)
+        loss.backward()
+        optimizer.step()
 
-# results per epoch
-@trainer.on(Events.EPOCH_COMPLETED)
-def log_training_results(trainer):
-    train_evaluator.run(train_loader_9less)
-    metrics = train_evaluator.state.metrics
-    print(f"Training Results - Epoch[{trainer.state.epoch}] Avg accuracy: {metrics['accuracy']:.2f} Avg loss: {metrics['loss']:.2f}")
+        running_loss += loss.item()
 
-# Define a Tensorboard logger
-tb_logger = TensorboardLogger(log_dir="./tb-logger")
+        if i % 10 == 9:  # Print every 10 batches
+            print(f'Epoch [{epoch+1}/{MAX_EPOCHS}], Step [{i+1}/{len(train_loader_9)}], Loss: {running_loss / 10:.4f}')
+            running_loss = 0.0
 
-tb_logger.attach(
-    trainer,
-    log_handler=OutputHandler(
-        tag="training",
-        metric_names=["accuracy", "loss"],
-        global_step_transform=global_step_from_engine(trainer)
-    ),
-    event_name=Events.EPOCH_COMPLETED
-)
-
-# Attach handler for plotting both evaluators' metrics after every epoch completes  
-#tb_logger.attach_output_handler(
-#    train_evaluator,
-#    event_name=Events.EPOCH_COMPLETED,
-#    tag="training",
-#    metric_names="all",
-#    global_step_transform=global_step_from_engine(trainer),
-#)
-
-### run trainer ###
-trainer.run(train_loader_9less, max_epochs=MAX_EPOCHS)
-
-# close tensorboard logger
-tb_logger.close()
+writer.flush()
+print('Finished Training')
