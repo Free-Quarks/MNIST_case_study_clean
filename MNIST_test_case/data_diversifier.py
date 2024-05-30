f105119ff7cc104b95fa1ea7e66b63fa412214a3
Free-Quarks <lieffers@arizona.edu>
2024-05-30 14:38:29 -0700

    wip

--- MNIST_test_case/data_diversifier.py
+++ MNIST_test_case/data_diversifier.py
@@ -1,6 +1,7 @@
 import torch
 from torch.distributions.multivariate_normal import MultivariateNormal
 from torch.distributions.kl import kl_divergence
+from torch.utils.data import ConcatDataset
 from tqdm import tqdm
 import pandas as pd
 from train import DatasetWrapper, INPUT_DIM
@@ -8,6 +9,7 @@ import time
 
 # config
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+MIN_KLD_THRESHOLD = 6
 
 
 def diversify_data(original_data, new_data, percent, maintain_size=True):
@@ -75,9 +77,101 @@ def diversify_data(original_data, new_data, percent, maintain_size=True):
         return diversified_data
 
 
+def diversify_data_precomputed(original_data, new_data, percent, diversity_matrix ,maintain_size=True):
+    """
+    This function will take in two dataset and diversify the original with data from the second. 
+    Using KLD as a metric of their encoding from a previously trained VAE encoder. This assumes the diversity
+    matrix has already been computed. 
+
+    Args:
+        original_data (dataset): the original dataset to be diversified
+        new_data (dataset): the new dataset from which we will diversify the original
+        percent (float): the percent of the new data to add to the original dataset
+        diversity_matrix (np-array): the diversity matrix of the KLD cross section between the orignal data and the new data
+        maintain_size (bool): should the original dataset maintain the same size (True) or do we simply add more data to it (False). 
+
+    Returns:
+        diversified_data (dataset): new diversified dataset
+    """
+
+    # number of points to find
+    num_of_new_data = int(len(new_data)*percent)
+
+    # get the most diverse points by index and min kld
+    min_list = []
+    for row in diversity_matrix:
+        min_list.append(min(row))
+
+    # assign indexes explicitly
+    new_points_idx = []
+    for i, kld in enumerate(min_list):
+        new_points_idx.append((i, kld))
+
+    # sort by min kld
+    new_points_idx.sort(key=lambda tup: tup[1], reverse=True)
+    
+    # only grab the number of new point indexes needed
+    new_points_idx = new_points_idx[0:num_of_new_data]
+
+    # actually grab the data
+    new_points = []
+    for idx, _kld in new_points_idx:
+        new_points.append(new_data[idx])
+
+    # convert it to torch Dataset
+    new_points = DatasetWrapper(new_points)
+
+    if maintain_size:
+        # need to remove data from original for each new point being added 
+        # we will remove based on being below a cut off since that is more efficient than finding the abolsute minimal 
+        # KLD overlap of all the original data, cut will be a KLD of 6, based on previous analysis
+        diversified_data = original_data
+
+        # load encoding model
+        encoding_model = torch.load('./MNIST_test_case/saved_models/vae_model.pt')
+        # need to shut off grads to parallelize inference and save space / computation
+        for p in encoding_model.parameters():
+            p.requires_grad = False
+        # move model to device and set up for eval
+        encoding_model.to(DEVICE)
+        encoding_model.eval() 
+
+        # find list of indicies of points that are similar, similar being min KLD below threshold
+        removal_list = []
+        original_data.to(DEVICE)
+        for i, (data, _) in tqdm(enumerate(original_data)):
+            mu1, logvar1 = encoding_model.encode(data.view(1, INPUT_DIM))
+            gaussian_1 = MultivariateNormal(mu1, torch.diagflat(torch.exp(logvar1)))
+            klds = []
+            for j, (data1, _)  in enumerate(original_data):
+                if j > i:
+                    mu, logvar = encoding_model.encode(data1.view(1, INPUT_DIM))
+                    gaussian = MultivariateNormal(mu, torch.diagflat(torch.exp(logvar)))
+                    klds.append(kl_divergence(gaussian, gaussian_1).cuda().item())
+            if min(klds) < MIN_KLD_THRESHOLD:
+                removal_list.append(i)
+            if len(removal_list) == num_of_new_data:
+                break
+
+        # remove the similar points
+        removal_list.sort(reverse=True)
+        for i in removal_list:
+            diversified_data.pop(i)
+
+        # add on the new points
+        diversified_data = ConcatDataset([diversified_data, new_points])
+        return diversified_data
+    else:
+        # just add the new data to the original data
+        diversified_data = ConcatDataset([original_data, new_points])
+        return diversified_data
 
 
 if __name__ == "__main__":
     train_dataset_9less = torch.load('./dataset/MNIST/modified/train_dataset_9less.pt')
     train_dataset_9 = torch.load('./dataset/MNIST/modified/train_dataset_9.pt')
-    diversify_data(train_dataset_9less, train_dataset_9, 0.1)
+    diversity_matrix = pd.read_parquet('./MNIST_test_case/saved_distributions/klds_matrix.parquet.snappy').to_numpy()
+    diverse_data = diversify_data_precomputed(train_dataset_9less, train_dataset_9, 0.02, diversity_matrix)
+    print(len(train_dataset_9less))
+    print(len(diverse_data))
+    print(f"{len(diverse_data)-len(train_dataset_9less)}")
--- MNIST_test_case/resnet.py
+++ MNIST_test_case/resnet.py
@@ -4,12 +4,15 @@ from torchvision.models import resnet18
 from train import DatasetWrapper # need this implicitly, i know it upsets linters
 from torch.utils.data import DataLoader, ConcatDataset
 from torch.utils.tensorboard import SummaryWriter
+from data_diversifier import diversify_data_precomputed
+import pandas as pd
 
 # config
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu') # 'cuda:0' if only one gpu
 MAX_EPOCHS = 5
 BATCH_SIZE = 128
 LR_RATE = 3e-4
+PERCENT_NEW_DATA = 0.2
 
 # define resnet18 model
 class R18(nn.Module):    
@@ -28,10 +31,16 @@ train_dataset_9less = torch.load('./dataset/MNIST/modified/train_dataset_9less.p
 train_dataset_9 = torch.load('./dataset/MNIST/modified/train_dataset_9.pt')
 train_dataset_all = ConcatDataset([train_dataset_9less, train_dataset_9])
 
+# get the diversified data, import the precomputed KLD cross section to dave time
+diversity_matrix = pd.read_parquet('./MNIST_test_case/saved_distributions/klds_matrix.parquet.snappy').to_numpy()
+diverse_dataset_larger = diversify_data_precomputed(train_dataset_9less, train_dataset_9, PERCENT_NEW_DATA, diversity_matrix, maintain_size=False)
+diverse_dataset = diversify_data_precomputed(train_dataset_9less, train_dataset_9, PERCENT_NEW_DATA, diversity_matrix)
+
 # load the data
 train_loader_9less = DataLoader(dataset=train_dataset_9less, batch_size=BATCH_SIZE, shuffle=True)
-train_loader_9 = DataLoader(dataset=train_dataset_9, batch_size=BATCH_SIZE, shuffle=True)
 train_loader_all = DataLoader(dataset=train_dataset_all, batch_size=BATCH_SIZE, shuffle=True)
+train_loader_diverse_larger = DataLoader(dataset=diverse_dataset_larger, batch_size=BATCH_SIZE, shuffle=True)
+train_loader_diverse = DataLoader(dataset=diverse_dataset, batch_size=BATCH_SIZE, shuffle=True)
 
 # initialize model and parallelization across GPU's
 model = R18()
