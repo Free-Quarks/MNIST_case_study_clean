4cc00c9c17b01075d7b583c64e4ee708195105fa
Free-Quarks <lieffers@arizona.edu>
2024-06-05 14:26:41 -0700

    cleaned up model comparison code and added f1 comparison

--- MNIST_test_case/model_comparison.py
+++ MNIST_test_case/model_comparison.py
@@ -5,235 +5,96 @@ from train import DatasetWrapper # need this implicitly to load dataset
 from resnet import R18
 from torch.utils.data import DataLoader
 from statistics import mean, stdev
+from torcheval.metrics.functional import multiclass_f1_score
+import os
 
 
 # config
-DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu') # 'cuda:0' if only one gpu
+DEVICE = torch.device('cuda:1' if torch.cuda.is_available() else 'cpu') # 'cuda:0' if only one gpu
 
-# import the test set
-test_dataset = torch.load('./dataset/MNIST/modified/test_set.pt')
-test_loader = DataLoader(dataset=test_dataset, batch_size=len(test_dataset))
+def compare_models(directory, test_dataset):
+    """
+    This function will take in a directory that contains several resnet models and a test dataset to evaluate the model on.
+    It then returns averages and stdev of certain metrics of the models compared together. 
 
-# import the models and set up the loss criterion, we annoyingly need to instatiate it multiple times
-model_9less_0 = R18()
-model_9less_1 = R18()
-model_9less_2 = R18()
-model_9less_3 = R18()
-model_all_0 = R18()
-model_all_1 = R18()
-model_all_2 = R18()
-model_all_3 = R18()
-model_all_norm_0 = R18()
-model_all_norm_1 = R18()
-model_all_norm_2 = R18()
-model_all_norm_3 = R18()
-#model_diverse_0 = R18()
-#model_diverse_1 = R18()
-#model_diverse_2 = R18()
-#model_diverse_3 = R18()
-model_diverse_larger_0 = R18()
-model_diverse_larger_1 = R18()
-model_diverse_larger_2 = R18()
-model_diverse_larger_3 = R18()
+    Args:
+        directory (string): the directory that contains the models we want to compare. The models should be state_dicts
+        test_dataset (dataset): the dataset which will be used to compare the models
 
-criterion = nn.CrossEntropyLoss()
+    Returns:
+        metrics (dict): a nested dict that contains the metrics calulated from the models compared. Loss and F1-Score here.
+    """
 
-# 9less data models
-model_9less_0.load_state_dict(torch.load("./MNIST_test_case/saved_models/9less/9less_0.pt"))
-model_9less_1.load_state_dict(torch.load("./MNIST_test_case/saved_models/9less/9less_1.pt"))
-model_9less_2.load_state_dict(torch.load("./MNIST_test_case/saved_models/9less/9less_2.pt"))
-model_9less_3.load_state_dict(torch.load("./MNIST_test_case/saved_models/9less/9less_3.pt"))
-model_9less_0.eval()
-model_9less_1.eval()
-model_9less_2.eval()
-model_9less_3.eval()
+    # load in the data and push them to the GPU
+    test_loader = DataLoader(dataset=test_dataset, batch_size=len(test_dataset))
+    for inputs, labels in test_loader:
+        inputs, labels  = inputs.to(DEVICE), labels.to(DEVICE)
 
-# all data models
-model_all_0.load_state_dict(torch.load("./MNIST_test_case/saved_models/all/all_0.pt"))
-model_all_1.load_state_dict(torch.load("./MNIST_test_case/saved_models/all/all_1.pt"))
-model_all_2.load_state_dict(torch.load("./MNIST_test_case/saved_models/all/all_2.pt"))
-model_all_3.load_state_dict(torch.load("./MNIST_test_case/saved_models/all/all_3.pt"))
-model_all_0.eval()
-model_all_1.eval()
-model_all_2.eval()
-model_all_3.eval()
+    walker = os.walk(directory)
+    criterion = nn.CrossEntropyLoss()
 
-# all normalized data amount models
-model_all_norm_0.load_state_dict(torch.load("./MNIST_test_case/saved_models/all_norm/all_norm_0.pt"))
-model_all_norm_1.load_state_dict(torch.load("./MNIST_test_case/saved_models/all_norm/all_norm_1.pt"))
-model_all_norm_2.load_state_dict(torch.load("./MNIST_test_case/saved_models/all_norm/all_norm_2.pt"))
-model_all_norm_3.load_state_dict(torch.load("./MNIST_test_case/saved_models/all_norm/all_norm_3.pt"))
-model_all_norm_0.eval()
-model_all_norm_1.eval()
-model_all_norm_2.eval()
-model_all_norm_3.eval()
+    losses = []
+    f1_scores = []
+    for _root, _dirs, models in walker:
+        for model in models:
+            # initialize and import the model
+            r18 = R18()
+            r18.load_state_dict(torch.load(directory+'/'+model))
+            r18.eval()
 
-# diverse normalized data models
-"""
-model_diverse_0.load_state_dict(torch.load("./MNIST_test_case/saved_models/diverse/diverse_0.pt"))
-model_diverse_1.load_state_dict(torch.load("./MNIST_test_case/saved_models/diverse/diverse_1.pt"))
-model_diverse_2.load_state_dict(torch.load("./MNIST_test_case/saved_models/diverse/diverse_2.pt"))
-model_diverse_3.load_state_dict(torch.load("./MNIST_test_case/saved_models/diverse/diverse_3.pt"))
-model_diverse_0.eval()
-model_diverse_1.eval()
-model_diverse_2.eval()
-model_diverse_3.eval()
-"""
+            #  push model to device and calculate metrics 
+            r18.to(DEVICE)
+            output = r18(inputs)
+            losses.append(criterion(output, labels).item())
+            f1_scores.append(multiclass_f1_score(output, labels, num_classes=10).item())
+            del output
+            del r18
 
-# diverse data models
-model_diverse_larger_0.load_state_dict(torch.load("./MNIST_test_case/saved_models/diverse_larger/diverse_larger_0.pt"))
-model_diverse_larger_1.load_state_dict(torch.load("./MNIST_test_case/saved_models/diverse_larger/diverse_larger_1.pt"))
-model_diverse_larger_2.load_state_dict(torch.load("./MNIST_test_case/saved_models/diverse_larger/diverse_larger_2.pt"))
-model_diverse_larger_3.load_state_dict(torch.load("./MNIST_test_case/saved_models/diverse_larger/diverse_larger_3.pt"))
-model_diverse_larger_0.eval()
-model_diverse_larger_1.eval()
-model_diverse_larger_2.eval()
-model_diverse_larger_3.eval()
+        avg_loss = mean(losses)
+        stdev_loss = stdev(losses)
+        
+        avg_f1_score = mean(f1_scores)
+        stdev_f1_score = stdev(f1_scores)
 
-# now to compute the loss on the test set, Test set should be batched as a single batch 
-for inputs, labels in test_loader:
-    inputs, labels  = inputs.to(DEVICE), labels.to(DEVICE)
+        del inputs, labels
 
-# initialize lists
-loss_9less = []
-loss_all = []
-loss_all_norm = []
-#loss_diverse = []
-loss_diverse_larger = []
+        metrics = {
+            'Loss' : {
+                'Avg' : avg_loss,
+                'Stdev' : stdev_loss,
+            },
+            'F1' : {
+                'Avg' : avg_f1_score,
+                'Stdev' : stdev_f1_score,
+            }
+        }
 
-# compute all the outputs, need to load and unload models and outputs for memory considerations
+    return metrics
 
-###### 9less models ######
-model_9less_0.to(DEVICE)
-output = model_9less_0(inputs)
-loss_9less.append(criterion(output, labels).item())
-del output
+if __name__ == "__main__":
+    # import the test set
+    test_dataset = torch.load('./dataset/MNIST/modified/test_set.pt')
 
-model_9less_1.to(DEVICE)
-output = model_9less_1(inputs)
-loss_9less.append(criterion(output, labels).item())
-del output
-
-model_9less_2.to(DEVICE)
-output = model_9less_2(inputs)
-loss_9less.append(criterion(output, labels).item())
-del output
-
-model_9less_3.to(DEVICE)
-output = model_9less_3(inputs)
-loss_9less.append(criterion(output, labels).item())
-del output
-
-# compute statistics here so gc can clear memory
-avg_loss_9less = mean(loss_9less)
-stdev_loss_9less = stdev(loss_9less)
-
-###### all models ######
-model_all_0.to(DEVICE)
-output = model_all_0(inputs)
-loss_all.append(criterion(output, labels).item())
-del output
-
-model_all_1.to(DEVICE)
-output = model_all_1(inputs)
-loss_all.append(criterion(output, labels).item())
-del output
-
-model_all_2.to(DEVICE)
-output = model_all_2(inputs)
-loss_all.append(criterion(output, labels).item())
-del output
-
-model_all_3.to(DEVICE)
-output = model_all_3(inputs)
-loss_all.append(criterion(output, labels).item())
-del output
-
-# compute statistics here so gc can clear memory
-avg_loss_all = mean(loss_all)
-stdev_loss_all = stdev(loss_all)
-
-###### all_norm models ######
-model_all_norm_0.to(DEVICE)
-output = model_all_norm_0(inputs)
-loss_all_norm.append(criterion(output, labels).item())
-del output
-
-model_all_norm_1.to(DEVICE)
-output = model_all_norm_1(inputs)
-loss_all_norm.append(criterion(output, labels).item())
-del output
-
-model_all_norm_2.to(DEVICE)
-output = model_all_norm_2(inputs)
-loss_all_norm.append(criterion(output, labels).item())
-del output
-
-model_all_norm_3.to(DEVICE)
-output = model_all_norm_3(inputs)
-loss_all_norm.append(criterion(output, labels).item())
-del output
-
-# compute statistics here so gc can clear memory
-avg_loss_all_norm = mean(loss_all_norm)
-stdev_loss_all_norm = stdev(loss_all_norm)
-
-"""
-###### diverse models ######
-model_diverse_0.to(DEVICE)
-output = model_diverse_0(inputs)
-loss_diverse.append(criterion(output, labels).item())
-del output
-
-model_diverse_1.to(DEVICE)
-output = model_diverse_1(inputs)
-loss_diverse.append(criterion(output, labels).item())
-del output
-
-model_diverse_2.to(DEVICE)
-output = model_diverse_2(inputs)
-loss_diverse.append(criterion(output, labels).item())
-del output
-
-model_diverse_3.to(DEVICE)
-output = model_diverse_3(inputs)
-loss_diverse.append(criterion(output, labels).item())
-del output
-
-# compute statistics here so gc can clear memory
-avg_loss_diverse = mean(loss_diverse)
-stdev_loss_diverse = stdev(loss_diverse)
-"""
-
-###### diverse_larger models ######
-model_diverse_larger_0.to(DEVICE)
-output = model_diverse_larger_0(inputs)
-loss_diverse_larger.append(criterion(output, labels).item())
-del output
-
-model_diverse_larger_1.to(DEVICE)
-output = model_diverse_larger_1(inputs)
-loss_diverse_larger.append(criterion(output, labels).item())
-del output
-
-model_diverse_larger_2.to(DEVICE)
-output = model_diverse_larger_2(inputs)
-loss_diverse_larger.append(criterion(output, labels).item())
-del output
-
-model_diverse_larger_3.to(DEVICE)
-output = model_diverse_larger_3(inputs)
-loss_diverse_larger.append(criterion(output, labels).item())
-del output
-
-# compute statistics here so gc can clear memory
-avg_loss_diverse_larger = mean(loss_diverse_larger)
-stdev_loss_diverse_larger = stdev(loss_diverse_larger)
+    # set up subdirectories to be walked over
+    subdirectories = ['9less', 'all', 'all_norm', 'diverse_larger'] # need to add 'diverse when we get the data
+    model_metrics = {}
+    for dir in subdirectories:
+        full_dir = "./MNIST_test_case/saved_models/" + dir
+        model_metrics[dir] = compare_models(full_dir, test_dataset)
 
 
-# print results
-print(f"avg loss 9less: {avg_loss_9less} +/- {stdev_loss_9less}")
-print(f"avg loss all: {avg_loss_all} +/- {stdev_loss_all}")
-print(f"avg loss all_norm: {avg_loss_all_norm} +/- {stdev_loss_all_norm}")
-#print(f"avg loss diverse: {avg_loss_diverse} +/- {stdev_loss_diverse}")
-print(f"avg loss diverse_larger: {avg_loss_diverse_larger} +/- {stdev_loss_diverse_larger}")
+    # print results
+    print('--------------------Losses------------------------')
+    print(f"avg loss 9less: {model_metrics['9less']['Loss']['Avg']} +/- {model_metrics['9less']['Loss']['Stdev']}")
+    print(f"avg loss all: {model_metrics['all']['Loss']['Avg']} +/- {model_metrics['all']['Loss']['Stdev']}")
+    print(f"avg loss all_norm: {model_metrics['all_norm']['Loss']['Avg']} +/- {model_metrics['all_norm']['Loss']['Stdev']}")
+    #print(f"avg loss diverse: {model_metrics['diverse']['Loss']['Avg']} +/- {model_metrics['diverse']['Loss']['Stdev']}")
+    print(f"avg loss diverse_larger: {model_metrics['diverse_larger']['Loss']['Avg']} +/- {model_metrics['diverse_larger']['Loss']['Stdev']}")
+    print('--------------------F1 Scores------------------------')
+    print(f"avg f1 9less: {model_metrics['9less']['F1']['Avg']} +/- {model_metrics['9less']['F1']['Stdev']}")
+    print(f"avg f1 all: {model_metrics['all']['F1']['Avg']} +/- {model_metrics['all']['F1']['Stdev']}")
+    print(f"avg f1 all_norm: {model_metrics['all_norm']['F1']['Avg']} +/- {model_metrics['all_norm']['F1']['Stdev']}")
+    #print(f"avg f1 diverse: {model_metrics['diverse']['F1']['Avg']} +/- {model_metrics['diverse']['F1']['Stdev']}")
+    print(f"avg f1 diverse_larger: {model_metrics['diverse_larger']['F1']['Avg']} +/- {model_metrics['diverse_larger']['F1']['Stdev']}")
+    print('--------------------------------------------')
+    print(model_metrics)
