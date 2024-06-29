e01f9dcbb0b0e537dcf639debfa6f9eb9650cfb8
Free-Quarks <lieffers@arizona.edu>
2024-06-29 15:26:26 -0700

    added initial classification models for tree and tokens, plus training stub for them

---
+++ code_case/classification_models.py
@@ -0,0 +1,72 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+from torch_geometric.nn import MessagePassing, GCNConv, aggr
+from tree_encoder_model import MPNNLayer
+
+# config
+
+
+class TreeClassificationModel(nn.Module):
+    def __init__(self, embed_dim, in_channels, hidden_channels, out_channels, node_classes, compressed_classes, graph_classes):
+        super(TreeClassificationModel, self).__init__()
+        # graph encoder layers
+        self.embed_contraction = nn.Linear(embed_dim, in_channels)
+        self.mpnn_feature = MPNNLayer(in_channels, hidden_channels)
+        self.gcn_feature = GCNConv(hidden_channels, out_channels)
+        self.mpnn_class = MPNNLayer(node_classes, compressed_classes)
+        self.gcn_class = GCNConv(compressed_classes, out_channels)
+        self.attention_nn = nn.Sequential(
+            nn.Linear(out_channels, out_channels),
+            nn.ReLU(),
+            nn.Linear(out_channels, out_channels)
+        )
+        self.attention_pooling = aggr.AttentionalAggregation(self.attention_nn)
+        self.attention_2_class = nn.Linear(out_channels, graph_classes)
+
+    def forward(self, x1, x2, pe, edge_index, batch):
+        # encode the embedding feature dimension
+        x1 = self.embed_contraction(x1)
+        x1_res = F.relu(x1)
+        # 3x concatenated pe, so we can use smaller pe dim but also not too small of embedding dim
+        x1 = x1_res + torch.cat((pe, pe, pe), dim=-1) 
+        x1 = self.mpnn_feature(x1, edge_index)
+        x1 = F.relu(x1)
+        x1 = self.gcn_feature(x1, edge_index)
+        x1 = F.relu(x1)
+        # encode the node class dimension
+        x2 = self.mpnn_class(x2, edge_index)
+        x2 = F.relu(x2)
+        x2 = self.gcn_class(x2, edge_index)
+        x2 = F.relu(x2)
+
+        # combine the two feature vectors and a residual connection to contracted output
+        z = x1 + x2 + x1_res # 4x cat'ing node class to match dimensions
+        # global attention pooling
+        z = self.attention_pooling(z, batch) # batch is an actual attribute of the data object in pytorch geometric
+        z = F.relu(z)
+        z = self.attention_2_class(z)
+        z = F.softmax(z)
+
+
+class TokenClassificationModel(nn.Module):
+    def __init__(self, embed_dim, in_channels, hidden_layer1, hidden_layer2, graph_classes):
+        super(TokenClassificationModel, self).__init__()
+        self.embed_contraction = nn.Linear(embed_dim, in_channels)
+        self.hidden_layer1 = nn.Linear(in_channels, hidden_layer1)
+        self.hidden_layer2 = nn.Linear(hidden_layer1, hidden_layer2)
+        self.hidden_layer3 = nn.Linear(hidden_layer2, hidden_layer2)
+        self.hidden_2_class = nn.Linear(hidden_layer2, graph_classes)
+
+    def forward(self, x):
+        x = self.embed_contraction(x)
+        x_res = F.relu(x)
+        x = self.hidden_layer1(x_res)
+        x = F.relu(x)
+        x = self.hidden_layer2(x)
+        x = F.relu(x)
+        x = self.hidden_layer3(x)
+        x = F.relu(x)
+        x = x + x_res
+        x = self.hidden_2_class(x)
+        z = F.softmax(x)
---
+++ code_case/classification_trainer.py
@@ -0,0 +1,57 @@
+from classification_models import TreeClassificationModel, TokenClassificationModel
+from code_preprocessing import preprocess_tree, ListDatasetWrapper
+import torch
+from torch import nn, optim
+from torch.utils.data import Dataset
+from tqdm import tqdm
+from torch_geometric.loader import DataLoader
+
+
+# CONFIG
+# Architecture Configs
+EMBED_DIM = 256 # fixed by codet5p
+IN_CHANNELS = 36 # empirical, might be too small
+HIDDEN_CHANNELS = 36 # empirical, might be too small
+OUT_CHANNELS = 36 # empirical, might be too small
+NODE_CLASSES = 10 # fixed by number of node states
+COMPRESSED_CLASSES = 6 # empirical
+COMPRESSED_GRAPH_FEATURE = 24 # empirical, might be too small
+HIDDEN_LAYER1 = 32
+HIDDEN_LAYER2 = 24
+GRAPH_CLASSES = 2 
+
+# Training Configs
+BATCH_SIZE = 8 # empirical, based on most graphs have different shapes, so hard to batch effectively
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu') # 'cuda:0' if only one gpu
+MAX_EPOCHS = 5
+LR_RATE = 3e-4
+
+if __name__ == "__main__":
+
+    # This is for the tree based classification model training, not proper data yet, but decent stub for now
+
+    # load the data in
+    code_directory = "./dataset/test_code"
+    fn_directory = "./dataset/test_fn"
+    tree_dataset = preprocess_tree(fn_directory, code_directory)
+    train_loader = DataLoader(dataset=tree_dataset, batch_size=BATCH_SIZE, shuffle=True) # batch the data
+
+    # load model, optimizer, and loss function
+    model = TreeClassificationModel(EMBED_DIM, IN_CHANNELS, HIDDEN_CHANNELS, OUT_CHANNELS, NODE_CLASSES, COMPRESSED_CLASSES, GRAPH_CLASSES).to(DEVICE) # load model to device
+    optimizer = optim.Adam(model.parameters(), lr=LR_RATE) # adam optimizer
+    loss_function = nn.BCELoss()
+    model.train()
+
+    # training the model
+    for epoch in range(1, MAX_EPOCHS):
+        overall_loss = 0
+        for batch_idx, data in tqdm(enumerate(train_loader)):
+            # need to use data.shape[0] as batch size in view because dataset no longer evenly divisble by 32
+            data = data.to(DEVICE)
+            optimizer.zero_grad()
+            recon_batch = model(data.x1, data.x2, data.pe, data.edge_index, data.batch)
+            loss = loss_function(recon_batch, data.x1)
+            overall_loss += loss.item()
+            loss.backward()
+            optimizer.step()
+        print("\tEpoch", epoch + 1, "\tAverage Loss: ", overall_loss/((batch_idx+1)*BATCH_SIZE))
