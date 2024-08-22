45ff3c3ad69e7aa548b875a7741a77b0d495cbb7
Free-Quarks <lieffers@arizona.edu>
2024-08-22 12:52:52 -0700

    metrics calculation funtions

--- code_case/metrics.py
+++ code_case/metrics.py
@@ -28,9 +28,45 @@ from vector_db import ChromaCodet5pEmbedding, ChromaTreeEmbedding
 from tree_model_trainer import EMBED_DIM, IN_CHANNELS, HIDDEN_CHANNELS, OUT_CHANNELS, NODE_CLASSES, COMPRESSED_CLASSES, COMPRESSED_GRAPH_FEATURE, GRAPH_FEATURE
 
 class CollectionStats:
-    def __init__(self, collection_name, ):
+    def __init__(self, collection_name):
         self.collection_name = collection_name
-        
+        self.class_mean_vecs = []
+        self.class_means = []
+        self.mean_vec = []
+        self.mean = 0
+        self.class_stdev_vecs = []
+        self.class_stdevs = []
+        self.stdev_vec = []
+        self.stdev = 0
+        self.agg_stdev = 0
+        self.avg_stdev = 0
+        self.bimod = 0
+        self.skewness_vec = []
+        self.skewness = 0
+        self.kurtosis_vec = []
+        self.kurtosis = 0
+
+    def get_stats(self, collection):
+        means, mean_vecs, stdevs, stdev_means, agg_stdev = agg_std_dev(collection)
+        avg_stdev = avg_std_dev(collection)
+        mean, mean_mean, stdev, stdev_mean = std_dev(collection)
+        g, g_mean, k, k_mean, bimod = bimodality(collection)
+
+        self.class_mean_vecs = mean_vecs
+        self.class_means = means
+        self.mean_vec = mean
+        self.mean = mean_mean
+        self.stdev_vec = stdev
+        self.stdev = stdev_mean
+        self.class_stdev_vecs = stdevs
+        self.class_stdevs = stdev_means
+        self.agg_stdev = agg_stdev
+        self.avg_stdev = avg_stdev
+        self.bimod = bimod
+        self.skewness_vec = g
+        self.skewness = g_mean
+        self.kurtosis_vec = k
+        self.kurtosis = k_mean        
 
 
 def vecdb_2_labeled_embeddings(dict):
@@ -68,12 +104,32 @@ def agg_std_dev(collection):
     class_0_array = np.array(class_0)
     class_1_array = np.array(class_1)
 
-    mean_0 = np.mean(class_0_array)
-    mean_1 = np.mean(class_1_array)
+    means = []
+    mean_vecs = []
+    stdevs = []
+    stdev_means = []
+
+    mean_0_vec = np.mean(class_0_array)
+    mean_0 = np.mean(mean_0_vec)
+    mean_1_vec = np.mean(class_1_array)
+    mean_1 = np.mean(mean_1_vec)
     stdev_0 = np.std(class_0_array, axis=0)
     stdev_1 = np.std(class_1_array, axis=0)
     stdev_mean_0 = np.mean(stdev_0)
     stdev_mean_1 = np.mean(stdev_1)
+
+    means.append(mean_0)
+    means.append(mean_1)
+    mean_vecs.append(mean_0_vec)
+    mean_vecs.append(mean_1_vec)
+    stdevs.append(stdev_0)
+    stdevs.append(stdev_1)
+    stdev_means.append(stdev_mean_0)
+    stdev_means.append(stdev_mean_1)
+
+    agg_stdev = stdev_mean_0 + stdev_mean_1
+
+    return (means, mean_vecs, stdevs, stdev_means, agg_stdev)
 
 def avg_std_dev(collection):
     # this is class-specific so we need to read in the class too
@@ -91,12 +147,14 @@ def avg_std_dev(collection):
     class_0_array = np.array(class_0)
     class_1_array = np.array(class_1)
 
-    mean_0 = np.mean(class_0_array)
-    mean_1 = np.mean(class_1_array)
     stdev_0 = np.std(class_0_array, axis=0)
     stdev_1 = np.std(class_1_array, axis=0)
     stdev_mean_0 = np.mean(stdev_0)
     stdev_mean_1 = np.mean(stdev_1)
+
+    avg_stdev = np.mean([stdev_mean_0,stdev_mean_1])
+
+    return avg_stdev
 
 def std_dev(collection):
     # this is class agnostic so just the data is fine
@@ -104,8 +162,11 @@ def std_dev(collection):
     embeddings, _labels = vecdb_2_labeled_embeddings(collection_meta)
 
     mean = np.mean(embeddings)
+    mean_mean = np.mean(mean)
     stdev = np.std(embeddings, axis=0)
-    stdev_mean_0 = np.mean(stdev)
+    stdev_mean = np.mean(stdev)
+
+    return (mean, mean_mean, stdev, stdev_mean)
 
 
 def bimodality(collection):
@@ -120,3 +181,10 @@ def bimodality(collection):
     k_mean = np.mean(k)
 
     bimod = (g_mean**2 + 1)/(k_mean + (3*(n-1)**2)/((n-2)*(n-3)))
+
+    return (g, g_mean, k, k_mean, bimod)
+
+if __name__ == "__main__":
+    # need to check that the data is being pulled correctly from the metadata in the vectordb
+    # need to check the few shot prompt is staying consistent with the class and overall prompt
+    print("temp")
